3daab431270ed211cc5d1a5fcd679394986b5e44
sumanjeet0012 <sumanjeet0012@gmail.com>
2026-07-13 19:10:45 +0530

    style: fix linting issues

--- tests/test_benchmarks.py
+++ tests/test_benchmarks.py
@@ -1,12 +1,15 @@
 import pytest
-from multibase import encode, decode, ENCODINGS
+
+from multibase import ENCODINGS, decode, encode
 
 BENCH_DATA = b"Decentralize everything!!!"
+
 
 @pytest.mark.benchmark
 @pytest.mark.parametrize("encoding_info", ENCODINGS, ids=lambda e: e.encoding)
 def test_bench_encode(benchmark, encoding_info):
     benchmark(encode, encoding_info.encoding, BENCH_DATA)
+
 
 @pytest.mark.benchmark
 @pytest.mark.parametrize("encoding_info", ENCODINGS, ids=lambda e: e.encoding)
@@ -14,9 +17,11 @@ def test_bench_decode(benchmark, encoding_info):
     encoded = encode(encoding_info.encoding, BENCH_DATA)
     benchmark(decode, encoded)
 
+
 @pytest.mark.benchmark
 @pytest.mark.parametrize("encoding_info", ENCODINGS, ids=lambda e: e.encoding)
 def test_bench_roundtrip(benchmark, encoding_info):
     def roundtrip():
         return decode(encode(encoding_info.encoding, BENCH_DATA))
+
     benchmark(roundtrip)
